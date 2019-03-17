838609ec103f6338d49b5978fb7742717a6f54a2
Colin White <clwhite814@gmail.com>
2019-03-17 10:37:56 -0500

    README.md added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace random_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
